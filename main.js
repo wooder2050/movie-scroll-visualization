081368d3c45f4bb9df023891c8dad9110c56fc3d
Chloe Noh <shwldus3@gmail.com>
2022-09-15 14:24:08 +0900

    ssv: 스크롤 스토리텔링 만들기

--- main.js
+++ main.js
@@ -1,83 +1,107 @@
 import * as d3 from "d3";
+import { scroller } from "./scroller.js";
+import { LineChart } from "./charts/line_chart.js";
+import {
+  getMovieData
+} from './services/movie_data.js';
 
-async function getData() {
-  const dateSet = new Set();
-  const csv = await d3.csv(`./data/movie2019.csv`);
-  const data = csv.map(d => {
-    const date = new Date(d.date);
-    dateSet.add(date);
-    d.date = date;
-    d.value = parseInt(d.value);
-    return d;
-  }).sort((a, b) => a.date - b.date);
-  const dates = Array.from(dateSet).sort();
+const WIDTH = 1000;
+const HEIGHT = 1000;
 
-  return {data, dates}
-}
+let lineChart;
 
-async function makeChart() {
-  const width = 1000; 
-  const height = 1000;
-  const margin = { top: 80, right: 30, bottom: 100, left: 80 };
+setTimeout(drawInitial(), 100);
 
-  const { data, dates } = await getData();
+/*
+  drawInitial 함수
+    : 초기설정을 처리합니다. 
+    1) visbox 너비와 높이 설정
+    2) 차트 클래스의 인스턴스 생성
+*/
+function drawInitial() {
+  d3.select("#visbox")
+    .append("svg")
+    .attr("width", WIDTH)
+    .attr("height", HEIGHT)
+    .attr("opacity", 1);
 
-  const body = d3.select("body");
-  const svg = body.append("svg")
-      .attr("width", width)
-      .attr("height", height);
+  lineChart = new LineChart('line-chart', WIDTH, HEIGHT)
 
-  // 아래 코드를 완성해보세요
-
-  // 1. x 척도 구현하기
-  //  - 시간척도(UTC)를 사용
-  //  - domain : dates 날짜 최소최대
-  //  - range : [margin.left, width - margin.right] 
-  const xScale =
-
-
-  // 1. y 척도 구현하기
-  //  - 선형척도를 사용
-  //  - domain : data의 value 최소최대
-  //  - range : [height - margin.bottom, margin.top] 
-  const yScale =
-
- 
-  // 2. x 축 구현하기
-  //  - 아래(Bottom)에 위치
-  //  - xScale (x척도)
-  const xAxis = 
-
-
-  // 2. y 축 구현하기
-  //  - 왼쪽(Left)에 위치
-  //  - yScale (y척도)
-  const yAxis =
-
-
-  svg.append('g')
-    .attr('transform', 'translate(0,' + (height - margin.bottom) + ')')
-    .call(xAxis);
-
-  svg.append('g')
-    .attr('transform', 'translate(' + margin.left + ',0)')
-    .call(yAxis);
-
-
-  // 3. 라인 차트 그리기
-  //  1) d3.line과 x, y메소드 구현
-  //    - x {key}는 'date'
-  //    - y {key}는 'value'
-  const line = 
-
-
-  //  2) svg에 path 요소와 속성 추가
-  //    - d : 2번에서 반환된 값
-  //    - fill : 'none'
-  //    - stroke : d3.schemeTableau10[0]
-  svg.append(
-
+  draw1();
 
 }
 
-makeChart();
+
+/*
+  clean 함수
+    : 'visType'에 따라, 해당 화면에 보여줄 필요가 없는 시각화들을 (보이지 않도록) 가립니다.
+    ex) unshow() 메소드를 호출하면, .attr("opacity", 0); 를 처리함 
+*/
+function clean(visType) {
+  const svg = d3.select("#visbox").select("svg");
+
+  if (visType !== "isFirst") {
+    svg.selectAll(".movie-img").attr("opacity", 0);
+    console.log("clean first chart");
+  }
+  if (visType !== "isLine"){
+    lineChart.unshow();
+    console.log("clean line chart");
+  }
+}
+
+
+/*
+  draw1 ~ draw3
+    : visbox(우측 화면)에 보여지는 부분 
+    - draw1 : 사진 
+    - draw2~3 : 라인차트 
+ */
+function draw1() {
+  clean("isFirst");
+
+  const svg = d3.select("#visbox").select("svg");
+
+  if (!svg.select(".movie-img").empty()) {
+    svg.select(".movie-img").attr("opacity", 1);
+    return;
+  }
+
+  svg
+    .append("svg:image")
+    .attr("class", "movie-img")
+    .attr("xlink:href", "images/movie.png")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", "100%")
+    .attr("height", "92%");
+}
+
+
+async function draw2() {
+  clean("isLine");
+
+  const { dates, data } = await getMovieData('2019');
+  lineChart.drawChart(dates, data).show();
+}
+
+
+async function draw3() {
+  clean("isLine");
+
+  const { dates, data } = await getMovieData('2020');
+  lineChart.drawChart(dates, data).show();
+}
+
+
+/*
+  scroller에서 
+  visFuncList에 등록된 순서대로 
+  visbox sections에 맞춰 보여줌
+*/
+const visFuncList = [
+  draw1,
+  draw2,
+  draw3,
+];
+scroller(visFuncList)();
---
+++ scroller.js
@@ -0,0 +1,60 @@
+import * as d3 from "d3";
+
+export function scroller(visFuncList){
+    const sections = d3.selectAll('.step');
+    const containerStart = 0;
+    let currentIndex = -1
+    let sectionPositions = [];
+
+    function execute() {
+        // 3. 브라우저 크기가 변경되거나, 스크롤 할 때마다 감지하기
+        // [start]
+
+        // [end]
+
+        setSectionPositions();
+
+        const timer = d3.timer(function() {
+            showCurrentVis();
+            timer.stop();
+        });
+    }
+
+    function setSectionPositions() {
+        let startPos;
+        sectionPositions = [];
+
+        // 1. .step 위치 파악하기
+        // [start]
+
+        // [end]
+    }
+
+    function showCurrentVis() {
+        const pos = window.pageYOffset - 300 - containerStart;
+
+        // 2. 현재 위치 알아내기
+        // [start]
+
+        // [end]
+    
+        if (currentIndex !== sectionIndex){
+            activate(sectionIndex);
+            currentIndex = sectionIndex;
+        }
+    }
+
+    function activate(index) {
+        d3.selectAll(".step")
+            .transition()
+            .duration(500)
+            .style("opacity", function (d, i) {
+                return i === index ? 1 : 0.1;
+            });
+        
+        visFuncList[index]();
+    }
+
+    return execute;
+}
+
