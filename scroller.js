be81a861ce44d5f0337093c559e26d82c47c2a9a
Zeze(강재영) <wooder2050@gmail.com>
2022-09-16 15:09:37 +0900

    complete : ssv chart

--- scroller.js
+++ scroller.js
@@ -1,58 +1,67 @@
-export function scroller(visFuncList){
-    const sections = d3.selectAll('.step');
-    const containerStart = 0;
-    let currentIndex = -1
-    let sectionPositions = [];
+export function scroller(visFuncList) {
+  const sections = d3.selectAll(".step");
+  const containerStart = 0;
+  let currentIndex = -1;
+  let sectionPositions = [];
 
-    function execute() {
-        // 3. 브라우저 크기가 변경되거나, 스크롤 할 때마다 감지하기
-        // [start]
+  function execute() {
+    // 3. 브라우저 크기가 변경되거나, 스크롤 할 때마다 감지하기
+    // [start]
+    d3.select(window)
+      .on("scroll.scroller", showCurrentVis)
+      .on("resize.scroller", setSectionPositions);
+    // [end]
 
-        // [end]
+    setSectionPositions();
 
-        setSectionPositions();
+    const timer = d3.timer(function () {
+      showCurrentVis();
+      timer.stop();
+    });
+  }
 
-        const timer = d3.timer(function() {
-            showCurrentVis();
-            timer.stop();
-        });
+  function setSectionPositions() {
+    let startPos;
+    sectionPositions = [];
+
+    // 1. .step 위치 파악하기
+    // [start]
+    d3.selectAll(".step").each(function (d, i) {
+      const top = this.getBoundingClientRect().top;
+
+      if (i === 0) {
+        startPos = top;
+      }
+      sectionPositions.push(top - startPos);
+    });
+    // [end]
+  }
+
+  function showCurrentVis() {
+    const pos = window.pageYOffset - 300 - containerStart;
+
+    // 2. 현재 위치 알아내기
+    // [start]
+    let sectionIndex = d3.bisect(sectionPositions, pos);
+    sectionIndex = Math.min(d3.selectAll(".step").size() - 1, sectionIndex);
+    // [end]
+
+    if (currentIndex !== sectionIndex) {
+      activate(sectionIndex);
+      currentIndex = sectionIndex;
     }
+  }
 
-    function setSectionPositions() {
-        let startPos;
-        sectionPositions = [];
+  function activate(index) {
+    d3.selectAll(".step")
+      .transition()
+      .duration(500)
+      .style("opacity", function (d, i) {
+        return i === index ? 1 : 0.1;
+      });
 
-        // 1. .step 위치 파악하기
-        // [start]
+    visFuncList[index]();
+  }
 
-        // [end]
-    }
-
-    function showCurrentVis() {
-        const pos = window.pageYOffset - 300 - containerStart;
-
-        // 2. 현재 위치 알아내기
-        // [start]
-
-        // [end]
-    
-        if (currentIndex !== sectionIndex){
-            activate(sectionIndex);
-            currentIndex = sectionIndex;
-        }
-    }
-
-    function activate(index) {
-        d3.selectAll(".step")
-            .transition()
-            .duration(500)
-            .style("opacity", function (d, i) {
-                return i === index ? 1 : 0.1;
-            });
-        
-        visFuncList[index]();
-    }
-
-    return execute;
+  return execute;
 }
-
