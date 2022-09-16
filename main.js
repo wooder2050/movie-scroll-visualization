95bd23a10ea1a9bf8495a2556ef1028491604e00
Zeze(강재영) <wooder2050@gmail.com>
2022-09-16 14:50:52 +0900

    complete: d3_01

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ async function getData() {
 }
 
 async function makeChart() {
-  const width = 1000; 
+  const width = 1000;
   const height = 1000;
   const margin = { top: 80, right: 30, bottom: 100, left: 80 };
 
@@ -30,21 +30,24 @@ async function makeChart() {
   // 1. x 척도 구현하기
   //  - 시간척도(UTC)를 사용
   //  - domain : d3.extent(dates)
-  //  - range : [margin.left, width - margin.right] 
-  const xScale =
+  //  - range : [margin.left, width - margin.right]
+  const xScale = d3.scaleUtc()
+	.domain(d3.extent(dates))
+	.range([margin.left, width - margin.right]);
 
 
   // 1. y 척도 구현하기
   //  - 선형척도를 사용
   //  - domain : [0, d3.max(data, d => d.value)]
-  //  - range : [height - margin.bottom, margin.top] 
-  const yScale =
+  //  - range : [height - margin.bottom, margin.top]
+  const yScale =d3.scaleLinear().domain( [0, d3.max(data, d => d.value)]
+  )	.range([height - margin.bottom, margin.top]);
 
 
   // 2. x 축 구현하기
   //  - 아래(Bottom)에 위치
   //  - xScale (x척도)
-  const xAxis = 
+  const xAxis =
 
 
   // 2. y 축 구현하기
@@ -66,19 +69,19 @@ async function makeChart() {
   //  1) d3.line과 x, y메소드 구현
   //    - x {key}는 'date'
   //    - y {key}는 'value'
-  const line = 
+  const line =
 
 
   //  2) svg에 path 요소와 속성 추가
   //   (svg는 d3.select(’svg’)로 위에서 먼저 할당된 변수입니다.)
   //    - path 요소 추가
   //    - data binding : data 변수
-  //    - d 속성 : line 변수  
+  //    - d 속성 : line 변수
   //    - fill 속성 : 'none'
   //    - stroke 속성 : d3.schemeTableau10[0]
-  svg.append(
+  svg.append()
 
-    
+
 }
 
 makeChart();
